fb095ac2d0272865a7bfc16f864ddd8b0f81c811
Shaun Johnson <shaun@lmxm.net>
2011-08-23 09:28:05 -0400

    Added ModuleType and Project entries into BootStrap.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,23 @@
+import net.lmxm.carm.domains.ModuleType
+import net.lmxm.carm.domains.Project
 import net.lmxm.carm.domains.SourceControlServer
 
 class BootStrap {
     def init = { servletContext ->
-        new SourceControlServer(name: "Subversion", type: "SUBVERSION", description: "Subversion server").save()
+        new SourceControlServer(name: "Subversion", type: SourceControlServerType.Subversion, 
+            description: "Subversion server", url: "http://suafe.googlecode.com/svn/").save()
+        
+        new ModuleType(name: "ColdFusion", description: "ColdFushion Web Application").save()
+        new ModuleType(name: "EJB", description: "Enterprise Java Bean Application").save()
+        new ModuleType(name: "JSF Portlet", description: "JSF Portlet Application").save()
+        new ModuleType(name: "JSF Web App", description: "JSF Web Application").save()
+        new ModuleType(name: "MDB", description: "Message Driven Bean Application").save()
+        new ModuleType(name: "Standalone JAR", description: "Standalone Java Application").save()
+        
+        new Project(name: "Suafe", description: "Subversion User Authorization File Editor")
     }
 
     def destroy = {
     }
 }
+
