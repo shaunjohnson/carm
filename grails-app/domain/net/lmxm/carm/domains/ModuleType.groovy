7582047f8ff94268ac50dea056d88cc14942f7e8
Shaun Johnson <shaun@lmxm.net>
2011-08-23 09:27:34 -0400

    Added ModuleType domain and associated unit tests.

---
+++ grails-app/domain/net/lmxm/carm/domains/ModuleType.groovy
@@ -0,0 +1,15 @@
+package net.lmxm.carm.domains
+
+class ModuleType {
+    String name
+    String description
+
+    static constraints = {
+        name(minSize: 2, maxSize: 50, blank: false, unique: true)
+        description(maxSize: 4000, nullable: true)
+    }
+
+    public String toString() {
+        return "ModuleType [name='$name']";
+    }
+}
---
+++ test/unit/net/lmxm/carm/domains/ModuleTypeTests.groovy
@@ -0,0 +1,83 @@
+package net.lmxm.carm.domains
+
+import grails.test.*
+
+class ModuleTypeTests extends GrailsUnitTestCase {
+    def moduleType
+    
+    protected void setUp() {
+        super.setUp()
+        
+        mockForConstraintsTests(ModuleType)
+
+        moduleType = new ModuleType(name: 'Portlet')
+        mockForConstraintsTests(ModuleType, [moduleType])
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+    
+    void testDescriptionMaxSize() {
+        def testModuleType = new ModuleType(name: 'Test Type', description: "a" * 4001)
+        mockForConstraintsTests(ModuleType, [testModuleType])
+
+        assertFalse 'Should not be valid', testModuleType.validate()
+        assertEquals 'Description is too long', 'maxSize', testModuleType.errors['description']
+    }
+
+    void testDescriptionNullable() {
+        def testModuleType = new ModuleType(name: 'Test Type', description: null)
+        mockForConstraintsTests(ModuleType, [testModuleType])
+
+        assertTrue 'Should be valid', testModuleType.validate()
+    }
+
+    void testNameBlank() {
+        def testModuleType = new ModuleType(name: '')
+        mockForConstraintsTests(ModuleType, [testModuleType])
+
+        assertFalse 'Should not be valid', testModuleType.validate()
+        assertEquals 'Name is blank', 'blank', testModuleType.errors['name']
+    }
+
+    void testNameMinSize() {
+        def testModuleType = new ModuleType(name: 'a')
+        mockForConstraintsTests(ModuleType, [testModuleType])
+
+        assertFalse 'Should not be valid', testModuleType.validate()
+        assertEquals 'Name is too short', 'minSize', testModuleType.errors['name']
+    }
+
+    void testNameMaxSize() {
+        def testModuleType = new ModuleType(name: 'a' * 51)
+        mockForConstraintsTests(ModuleType, [testModuleType])
+
+        assertFalse 'Should not be valid', testModuleType.validate()
+        assertEquals 'Name is too long', 'maxSize', testModuleType.errors['name']
+    }
+
+    void testNameNullable() {
+        def testModuleType = new ModuleType(name: null)
+        mockForConstraintsTests(ModuleType, [testModuleType])
+
+        assertFalse 'Should not be valid', testModuleType.validate()
+        assertEquals 'Name is blank', 'nullable', testModuleType.errors['name']
+    }
+
+    void testNameUnique() {
+        def testModuleType = new ModuleType(name: 'Portlet')
+        mockForConstraintsTests(ModuleType, [testModuleType])
+
+        assertFalse 'Should not be valid', moduleType.validate()
+        assertEquals 'Name is not unique.', 'unique', moduleType.errors['name']
+
+        moduleType = new ModuleType(name: 'Portlet2')
+        assertTrue 'Should be valid', moduleType.validate()
+    }
+
+    void testToString() {
+        assertNotNull moduleType.toString()
+        assertTrue moduleType.toString() ==~ /.*Portlet.*/
+    }
+}
