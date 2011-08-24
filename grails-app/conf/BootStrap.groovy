a55cdfaaa6876902578cfaf1dc4843b6cb617674
Shaun Johnson <shaun@lmxm.net>
2011-08-24 09:48:25 -0400

    Create "admin" and "user" accounts.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,14 +15,17 @@ class BootStrap {
         def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
         def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 
-        def testUser = new User(username: 'me', enabled: true, password: 'password')
-        testUser.save(flush: true)
+        def adminUser = new User(username: 'admin', enabled: true, password: 'admin')
+        adminUser.save(flush: true)
+        UserRole.create adminUser, adminRole, true
+        
+        def userUser = new User(username: 'user', enabled: true, password: 'user')
+        userUser.save(flush: true)
+        UserRole.create userUser, userRole, true
 
-        UserRole.create testUser, adminRole, true
-
-        assert User.count() == 1
+        assert User.count() == 2
         assert Role.count() == 2
-        assert UserRole.count() == 1
+        assert UserRole.count() == 2
         
         //
         // Configure Domains
