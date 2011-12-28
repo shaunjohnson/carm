c65d5156999a935624022d119f1298fecfafdb2a
Shaun Johnson <shaun@lmxm.net>
2011-12-27 20:42:55 -0500

    Added controller and GSP for handling security errors gracefully.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,20 @@
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.acls.model.NotFoundException
+
 class UrlMappings {
 
-	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
+    static mappings = {
+        "/$controller/$action?/$id?" {
+            constraints {
+                // apply constraints here
+            }
+        }
 
-		"/"(controller:"/home")
-		"500"(view:'/error')
-	}
+        "/"(controller: "/home")
+
+        "403"(controller: "errors", action: "error403")
+        "500"(controller: "errors", action: "error500")
+        "500"(controller: "errors", action: "error403", exception: AccessDeniedException)
+        "500"(controller: "errors", action: "error403", exception: NotFoundException)
+    }
 }
