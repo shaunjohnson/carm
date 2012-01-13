9cdcf44d49e8b6656e80c7cdba73ca1caf1e7588
Shaun Johnson <shaun@lmxm.net>
2012-01-13 10:07:45 -0500

    Changed DataSource.groovy so that MySQL is only used for development. Test still uses hsqldb.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,19 +1,8 @@
 dataSource {
     pooled = true
-    driverClassName = "com.mysql.jdbc.Driver"
-    username = "root"
-    password = "CARM123"
-    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-    properties {
-        maxActive = 50
-        maxIdle = 25
-        minIdle = 5
-        initialSize = 5
-        minEvictableIdleTimeMillis = 60000
-        timeBetweenEvictionRunsMillis = 60000
-        maxWait = 10000
-        validationQuery = "select 1"
-    }
+    driverClassName = "org.hsqldb.jdbcDriver"
+    username = "sa"
+    password = ""
 }
 
 hibernate {
@@ -26,6 +15,22 @@ hibernate {
 environments {
     development {
         dataSource {
+            pooled = true
+            driverClassName = "com.mysql.jdbc.Driver"
+            username = "root"
+            password = "CARM123"
+            dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+            properties {
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 5
+                initialSize = 5
+                minEvictableIdleTimeMillis = 60000
+                timeBetweenEvictionRunsMillis = 60000
+                maxWait = 10000
+                validationQuery = "select 1"
+            }
+
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
             url = "jdbc:mysql://localhost/carmdev"
         }
