7dc8cd13243a52ff828b141c1939d256ab8fc996
Shaun Johnson <shaun@lmxm.net>
2012-02-03 11:00:11 -0500

    Moved breadcrumb related custom tags to separate taglib with a unique namespace.
    Created custom tags for display breadcrumb links and text. This cleans up the
    header and will allow for pulling breadcrumb text from a resources file.

---
+++ test/unit/carm/CarmBreadcrumbTagLibTests.groovy
@@ -0,0 +1,17 @@
+package carm
+
+import grails.test.*
+
+class CarmBreadcrumbTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
