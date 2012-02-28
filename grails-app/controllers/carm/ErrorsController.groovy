a57730a1d1625439c779724de2fc06a72d8cac55
Shaun Johnson <shaun@lmxm.net>
2012-02-28 10:44:22 -0500

    Added custom 404 error page.

--- grails-app/controllers/carm/ErrorsController.groovy
+++ grails-app/controllers/carm/ErrorsController.groovy
@@ -5,9 +5,11 @@ import grails.plugins.springsecurity.Secured
 @Secured(['permitAll'])
 class ErrorsController {
 
-   def error403 = {}
+    def error403 = {}
 
-   def error500 = {
-      render view: '/error'
-   }
+    def error404 = {}
+
+    def error500 = {
+        render view: '/error'
+    }
 }
